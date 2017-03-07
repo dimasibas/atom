acdc9522c7015a629bfb0e0eae41d5656904ee28
dimasibas <snuchibuchiinfo@mail.ru>
2017-03-07 20:41:33 +0300

    ListNode added

--- lecture03/src/main/java/ru/atom/list/ListNode.java
+++ lecture03/src/main/java/ru/atom/list/ListNode.java
@@ -3,5 +3,46 @@ package ru.atom.list;
 /**
  * Contains ref to next node, prev node and value
  */
+
+
 public class ListNode<E> {
+
+   private E value;
+   private ListNode<E> prev;
+   private ListNode<E> next;
+
+   public (ListNode(E value, ListNode<E> prev, ListNode<E> next)) {
+      this.value = value;
+      this.next = next;
+      this.prev = prev
+   }
+
+   public E getValue() {
+
+      return value;
+   }
+
+   public ListNode<E> getNext() {
+
+      return next;
+   }
+
+   public ListNode<E> getPrev() {
+
+      return prev;
+   }
+
+   public void setValue(E value) {
+      this.value = value;
+   }
+   public void setPrev(ListNode<E> prev) {  //сылка на переменную prev в листноде
+
+      this.previous = previous;
+   }
+
+   public void setNext(ListNode<E> next)
+   {
+      this.next = next;
+   }
+
 }
